03488f24b3be729091c2ec5fa3ba91ecbd5157a2
Fabian Zelaya <fabianzelaya@icloud.com>
2023-09-06 04:20:03 -0700

    Update

--- main.c
+++ main.c
@@ -1,18 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> // Include the standard input-output library for using printf.
 
 int main() {
-    // arr[3][4];
 
-    int rows = 3;
-    int columns = 4;
+    printf("1");
 
-    int totalElements = rows * columns;
-
-    printf("int arr[%d][%d]; can hold a total of %d elements.\n", rows, columns, totalElements);
-
-    /*
-     * When you run this program, it will calculate the total number of elements that the array int arr[3][4]; can hold (which is 12) and display the result as specified in the output.
-     */
-
+    // Return 0 to indicate successful program execution.
     return 0;
 }
+//FZ
---
+++ two-dimensional-array/two-dimensional-array.c
@@ -0,0 +1,23 @@
+//
+// Created by Fabian Zelaya on 9/6/23.
+//
+
+#include <stdio.h>
+
+int main() {
+    // Declare and initialize variables to represent the number of rows and columns.
+    int rows = 3;
+    int columns = 4;
+
+    // Calculate the total number of elements by multiplying the number of rows and columns.
+    int totalElements = rows * columns;
+
+    // Display the result using printf:
+    // The format string specifies how to format the output.
+    // %d is a placeholder for an integer, and it's replaced by the values of rows, columns, and totalElements.
+    printf("int arr[%d][%d]; can hold a total of %d elements.\n", rows, columns, totalElements);
+
+    // Return 0 to indicate successful program execution.
+    return 0;
+}
+//FZ
